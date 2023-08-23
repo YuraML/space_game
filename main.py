6dc39ec075ff9d54a6ab0a2a5d7a6a292c73f90a
YuraML <malashtayuriy@gmail.com>
2023-08-23 22:10:40 +0400

    add 5 animated stars

--- main.py
+++ main.py
@@ -1,27 +1,43 @@
-import time
+import asyncio
 import curses
+import time
+
+TIC_TIMEOUT = 0.1
+
+
+async def blink(canvas, row, column, symbol='*'):
+    while True:
+        canvas.addstr(row, column, symbol, curses.A_DIM)
+        for _ in range(20):
+            await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol)
+        for _ in range(3):
+            await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol, curses.A_BOLD)
+        for _ in range(5):
+            await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol)
+        for _ in range(3):
+            await asyncio.sleep(0)
 
 
 def draw(canvas):
-    row, column = (5, 20)
     canvas.border()
+    row = 5
+    columns = [5, 10, 15, 20, 25]
+    coroutines = [blink(canvas, row, column) for column in columns]
 
     while True:
-        canvas.addstr(row, column, '*', curses.A_DIM)
+        for coroutine in coroutines.copy():
+            try:
+                coroutine.send(None)
+            except StopIteration:
+                coroutines.remove(coroutine)
         canvas.refresh()
-        time.sleep(2)
-
-        canvas.addstr(row, column, '*')
-        canvas.refresh()
-        time.sleep(0.3)
-
-        canvas.addstr(row, column, '*', curses.A_BOLD)
-        canvas.refresh()
-        time.sleep(0.5)
-
-        canvas.addstr(row, column, '*')
-        canvas.refresh()
-        time.sleep(0.3)
+        time.sleep(TIC_TIMEOUT)
 
 
 if __name__ == '__main__':
