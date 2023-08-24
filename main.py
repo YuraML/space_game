3b4d3ec53303d45f09bfc76b9c622969e9841c31
YuraML <malashtayuriy@gmail.com>
2023-08-24 18:01:40 +0400

    add controllable spaceship

--- main.py
+++ main.py
@@ -3,7 +3,99 @@ import curses
 import random
 import time
 
+from itertools import cycle
+
 TIC_TIMEOUT = 0.1
+SPACE_KEY_CODE = 32
+LEFT_KEY_CODE = 260
+RIGHT_KEY_CODE = 261
+UP_KEY_CODE = 259
+DOWN_KEY_CODE = 258
+
+
+def read_controls(canvas):
+    rows_direction = columns_direction = 0
+    space_pressed = False
+
+    while True:
+        pressed_key_code = canvas.getch()
+
+        if pressed_key_code == -1:
+            break
+
+        if pressed_key_code == UP_KEY_CODE:
+            rows_direction = -1
+
+        if pressed_key_code == DOWN_KEY_CODE:
+            rows_direction = 1
+
+        if pressed_key_code == RIGHT_KEY_CODE:
+            columns_direction = 1
+
+        if pressed_key_code == LEFT_KEY_CODE:
+            columns_direction = -1
+
+        if pressed_key_code == SPACE_KEY_CODE:
+            space_pressed = True
+
+    return rows_direction, columns_direction, space_pressed
+
+
+def draw_frame(canvas, start_row, start_column, text, negative=False):
+    rows_number, columns_number = canvas.getmaxyx()
+
+    for row, line in enumerate(text.splitlines(), round(start_row)):
+        if row < 0:
+            continue
+
+        if row >= rows_number:
+            break
+
+        for column, symbol in enumerate(line, round(start_column)):
+            if column < 0:
+                continue
+
+            if column >= columns_number:
+                break
+
+            if symbol == ' ':
+                continue
+
+            if row == rows_number - 1 and column == columns_number - 1:
+                continue
+
+            symbol = symbol if not negative else ' '
+            canvas.addch(row, column, symbol)
+
+
+def get_frame_size(text):
+    lines = text.splitlines()
+    rows = len(lines)
+    columns = max([len(line) for line in lines])
+    return rows, columns
+
+
+async def animate_spaceship(canvas, frames_cycle):
+    rows, columns = canvas.getmaxyx()
+    row = rows // 2
+    column = columns // 2
+    frame = next(frames_cycle)
+
+    while True:
+        rows_direction, columns_direction, _ = read_controls(canvas)
+        frame_height, frame_width = get_frame_size(frame)
+        draw_frame(canvas, row, column, frame, negative=True)
+
+        new_row = min(max(row + rows_direction, 1), rows - frame_height - 1)
+        new_column = min(max(column + columns_direction, 1), columns - frame_width - 1)
+        if 0 <= new_row and new_row + frame_height <= rows:
+            row = new_row
+        if 0 <= new_column and new_column + frame_width <= columns:
+            column = new_column
+
+        frame = next(frames_cycle)
+        draw_frame(canvas, row, column, frame)
+        await asyncio.sleep(0)
 
 
 async def blink(canvas, row, column, symbol='*'):
@@ -56,23 +148,35 @@ async def fire(canvas, start_row, start_column, rows_speed=-0.3, columns_speed=0
         column += columns_speed
 
 
+def load_frames():
+    frames = []
+    for frame_num in range(1, 3):
+        with open(f"animations/rocket_frame_{frame_num}.txt", "r") as file:
+            frames.append(file.read())
+
+    return frames
+
+
 def draw(canvas):
     canvas.border()
     curses.curs_set(False)
+    canvas.nodelay(True)
     stars_amount = 100
-    max_y, max_x = canvas.getmaxyx()
+    rows, columns = canvas.getmaxyx()
 
     coroutines = [
         blink(
             canvas,
-            random.randint(1, max_y - 2),
-            random.randint(1, max_x - 2),
+            random.randint(1, rows - 2),
+            random.randint(1, columns - 2),
             symbol=random.choice('+*.:')
         ) for _ in range(stars_amount)
     ]
 
-    fire_animation = fire(canvas, max_y // 2, max_x // 2)
-    coroutines.append(fire_animation)
+    spaceship_frames = load_frames()
+    spaceship_cycle = cycle(spaceship_frames)
+    spaceship = animate_spaceship(canvas, spaceship_cycle)
+    coroutines.append(spaceship)
 
     while True:
         for coroutine in coroutines.copy():
