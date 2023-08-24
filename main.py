b705ad24f7ef3b5b89af2e8c13bf7eeec88bad9d
YuraML <malashtayuriy@gmail.com>
2023-08-24 14:49:58 +0400

    add fire animation

--- main.py
+++ main.py
@@ -28,20 +28,51 @@ async def blink(canvas, row, column, symbol='*'):
             await asyncio.sleep(0)
 
 
+async def fire(canvas, start_row, start_column, rows_speed=-0.3, columns_speed=0):
+    row, column = start_row, start_column
+
+    canvas.addstr(round(row), round(column), '*')
+    await asyncio.sleep(0)
+
+    canvas.addstr(round(row), round(column), 'O')
+    await asyncio.sleep(0)
+    canvas.addstr(round(row), round(column), ' ')
+
+    row += rows_speed
+    column += columns_speed
+
+    symbol = '-' if columns_speed else '|'
+
+    rows, columns = canvas.getmaxyx()
+    max_row, max_column = rows - 1, columns - 1
+
+    curses.beep()
+
+    while 0 < row < max_row and 0 < column < max_column:
+        canvas.addstr(round(row), round(column), symbol)
+        await asyncio.sleep(0)
+        canvas.addstr(round(row), round(column), ' ')
+        row += rows_speed
+        column += columns_speed
+
+
 def draw(canvas):
     canvas.border()
     curses.curs_set(False)
     stars_amount = 100
     max_y, max_x = canvas.getmaxyx()
-    
+
     coroutines = [
         blink(
             canvas,
-            random.randint(1, max_y - 1),
-            random.randint(1, max_x - 1),
+            random.randint(1, max_y - 2),
+            random.randint(1, max_x - 2),
             symbol=random.choice('+*.:')
         ) for _ in range(stars_amount)
     ]
+
+    fire_animation = fire(canvas, max_y // 2, max_x // 2)
+    coroutines.append(fire_animation)
 
     while True:
         for coroutine in coroutines.copy():
